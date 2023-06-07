beb493a5276824512efa52902558a57c33e0cd55
junaidsheroz <junaidsheroz213@gmail.com>
2023-06-07 08:38:56 -0400

    1

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function add() {
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     var result = parseInt(num1) + parseInt(num2);
-    document.getElementById("result").value = ;
+    document.getElementById("result").value = result;
 }
